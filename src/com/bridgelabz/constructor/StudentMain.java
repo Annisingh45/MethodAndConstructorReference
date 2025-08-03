4b3e4eedd3a1654f4fa8623ebe905127dc8ad49b
Annisingh45 <anni28132@gmail.com>
2025-08-03 12:47:10 +0530

    implementation of constructor reference

--- src/com/bridgelabz/constructor/StudentMain.java
+++ src/com/bridgelabz/constructor/StudentMain.java
@@ -5,11 +5,11 @@ import java.util.function.Supplier;
 public class StudentMain {
     public static void main(String[] args) {
         //Using lembda expression
-        Supplier<Student> supplier=()->new Student();
+        Supplier<Student> supplier = () -> new Student();
         System.out.println(supplier.get());
 
         //Using constructor reference
-        Supplier supplier1=Student ::new;
+        Supplier supplier1 = Student::new;
         System.out.println(supplier1.get());
     }
 }
